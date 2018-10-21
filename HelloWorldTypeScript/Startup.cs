6b297eb73b09c9d9f0084325b8defa53dd384768
Maxim Michtchenko <mmichtch@outlook.com>
2018-10-20 18:55:32 -0700

    Added MVC and implemented ts in home/index

--- HelloWorldTypeScript/Startup.cs
+++ HelloWorldTypeScript/Startup.cs
@@ -15,6 +15,7 @@ namespace HellowWorldTypeScript
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,13 +24,16 @@ namespace HellowWorldTypeScript
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseStaticFiles();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            }); 
+
         }
     }
 }
